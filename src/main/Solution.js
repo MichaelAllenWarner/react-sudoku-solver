cc7ec76dc96f917fbdb2b77b878378a46b21f477
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-03-14 09:29:01 -0400

    changed Solution to functional component

--- src/main/Solution.js
+++ src/main/Solution.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Solution extends Component {
-  render() {
-    const value = (this.props.status === 'solved')
-      ? this.props.solutionArray.join('')
-      : (this.props.status === 'invalid')
-        ? 'Invalid puzzle.'
-        : (this.props.status === 'solving')
-          ? 'Solving ...'
-          : '';
-    return (
-      <div>
-        <input
-          id="solution"
-          placeholder="Solution string will appear here."
-          value={value}
-          readOnly={true}
-        />
-      </div>
-    );
-  }
-}
+const Solution = props => {
+  const value = (props.status === 'solved')
+    ? props.solutionArray.join('')
+    : (props.status === 'invalid')
+      ? 'Invalid puzzle.'
+      : (props.status === 'solving')
+        ? 'Solving ...'
+        : '';
+
+  return (
+    <div>
+      <input
+        id="solution"
+        placeholder="Solution string will appear here."
+        value={value}
+        readOnly={true}
+      />
+    </div>
+  );
+};
 
 Solution.propTypes = {
   status: PropTypes.string.isRequired,
